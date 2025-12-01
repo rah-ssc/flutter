d807410cbe6e9bf6b67d136d93d7177b8297506e
Rahul Gupta <114154062+rah-ssc@users.noreply.github.com>
2025-12-01 15:17:21 +0530

    code update
    code update

--- my_login_app/lib/main.dart
+++ my_login_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
+import 'utils/auto_route_logger.dart';   // <-- Add this
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
-      home: const LoginPage(),
-      debugShowCheckedModeBanner: false,
-    );
+  return MaterialApp(
+  title: 'Login App',
+  theme: ThemeData(
+    primarySwatch: Colors.blue,
+    useMaterial3: true,
+  ),
+  debugShowCheckedModeBanner: false,
+  home: const LoginPage(),
+  navigatorObservers: [AutoRouteLogger()],
+);
   }
 }
+
